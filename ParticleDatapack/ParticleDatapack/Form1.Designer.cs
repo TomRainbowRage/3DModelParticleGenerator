fde13a6e6dad251bfc942ef262364cb1329d5b39
TomRainbowRage <103732473+TomRainbowRage@users.noreply.github.com>
2023-01-10 18:34:38 +0000

    Add files via upload

---
+++ ParticleDatapack/ParticleDatapack/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace ParticleDatapack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(380, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Obj File";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(61, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(313, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(61, 72);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(313, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "MTL File";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(380, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 20);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "browse";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(175, 213);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "generate";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(483, 46);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(184, 45);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(674, 50);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(67, 20);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(490, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Particle Scale";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(124, 184);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(173, 23);
+            this.progressBar1.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(61, 98);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(313, 20);
+            this.textBox4.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "OutputDir";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(380, 98);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 20);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "browse";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(82, 124);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(114, 20);
+            this.textBox5.TabIndex = 16;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Output name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ParticleDatapack/ParticleDatapack/Form1.cs
@@ -0,0 +1,426 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+
+namespace ParticleDatapack
+{
+    public partial class Form1 : Form
+    {
+        public int amountprogress = 0;
+
+        public int faceslength = 0;
+        public int verticylength = 0;
+        public int vnlength = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+            if (Properties.Settings.Default["FileOutput"] != null)
+            {
+                textBox4.Text = Properties.Settings.Default["FileOutput"].ToString();
+            }
+
+            if(Properties.Settings.Default["FileName"] != null)
+            {
+                textBox5.Text = Properties.Settings.Default["FileName"].ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int size = -1;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            List<string> lines = new List<string>();
+
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                textBox1.Text = file;
+
+                try
+                {
+                    foreach (string line in File.ReadAllLines(file))
+                    {
+                        lines.Add(line);
+                    }
+
+
+                    size = File.ReadAllLines(file).Length;
+                }
+                catch (IOException)
+                {
+                }
+
+
+                Console.WriteLine(size); // <-- Shows file size in debugging mode.
+                Console.WriteLine(result); // <-- For debugging use.
+                Console.WriteLine(lines[4]);
+
+                if (File.Exists(GetDirNoFile(openFileDialog1.FileName) + "model.mtl"))
+                {
+                    textBox2.Text = GetDirNoFile(openFileDialog1.FileName) + "model.mtl";
+                }
+            }
+            
+            
+
+            
+        }
+
+
+        private string CleanFileName(string input)
+        {
+            string[] slipraw = input.Split(Path.DirectorySeparatorChar);
+
+            string ou = slipraw[slipraw.Length - 1];
+
+            string[] nameraw = ou.Split('.');
+
+
+            return nameraw[0];
+
+        }
+
+        private string GetDirNoFile(string file)
+        {
+            string[] slipraw = file.Split(Path.DirectorySeparatorChar);
+
+            string news = "";
+
+            for (int i = 0; i < slipraw.Length; i++)
+            {
+                //your code here
+                if (i != slipraw.Length - 1)
+                {
+                    if (i == 0)
+                    {
+                        news += slipraw[i];
+                    }
+                    else if (i != 0)
+                    {
+                        news += (Path.DirectorySeparatorChar + slipraw[i]);
+                    }
+
+                }
+                else
+                {
+                    news += Path.DirectorySeparatorChar;
+                }
+            }
+
+            return news;
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if(textBox1.Text == "")
+            {
+                MessageBox.Show("You Must select a model");
+                return;
+            }
+            if(textBox5.Text == "")
+            {
+                MessageBox.Show("You Must select a file name");
+            }
+            amountprogress = 0;
+
+            string pathout = Path.Combine(textBox4.Text, textBox5.Text + ".mcfunction");
+
+            if (!File.Exists(pathout))
+            {
+                File.Create(pathout);
+                MessageBox.Show("Please try again, This is a common error \n (already being written to)");
+                return;
+            }
+
+            // Get the materials - using a Dictionary here because it maps keys to values
+            // e.g. the name of the material is the key, and its colour is the value.
+            // Makes it efficient and easy to look up a material name and get its colour.
+            Dictionary<string, OBJMat> objmats = GetMaterialsFromMTL(textBox2.Text);
+
+            //string s = "v -0.086489 1.865501 0.138342";
+            //string stringout = ConvertToCommand(s, textBox2.Text);
+
+            //string write = "line \n another line \n \n space";
+
+            //File.WriteAllText(pathout, write);
+
+            
+
+            List<string> writelines = new List<string>();
+            List<Vertex> vertices = new List<Vertex>();
+            OBJMat currentMaterial = new OBJMat { r = 0, b = 0, g = 0 };
+
+            // I changed the function of this loop so that it stores all the vertices for a part
+            // of the object, and then when it sees the 'usemtl' line, it gets the colour for the
+            // material and then writes all the vertices with that colour. Then it deletes the
+            // vertices list (makes a new empty list) so that those vertices don't appear in the
+            // next part of the object.
+
+
+
+            int o = 0;
+            foreach (string line in File.ReadLines(textBox1.Text))
+            {
+                if (line.Contains("v"))
+                {
+
+
+                    if (!line.Contains("vn"))
+                    {
+                        verticylength += 1;
+                        vertices.Add(new Vertex { line = line, material = currentMaterial }); // store the vertices for the object
+                        //writelines.Add(ConvertToCommand(line, textBox2.Text));
+                    }
+                    if (line.Contains("vn"))
+                    {
+                        vnlength += 1;
+                    }
+
+                }
+                else if (line.Contains("f "))
+                {
+                    faceslength += 1;
+                    string[] facesSplit = line.Split(' ');
+                    //Console.WriteLine(facesSplit[1]);
+                    int v1 = int.Parse(facesSplit[1].Split('/')[0]); // take the vertex before the '/' and convert to a number
+                    int v2 = int.Parse(facesSplit[2].Split('/')[0]);
+                    int v3 = int.Parse(facesSplit[3].Split('/')[0]);
+                    vertices[v1].material = currentMaterial; // Set the colour on the vertex
+                    vertices[v2].material = currentMaterial;
+                    vertices[v3].material = currentMaterial;
+                }
+                else if (line.Contains("usemtl"))
+                {
+                    string[] mtlsplit = line.Split(' ');
+                    try
+                    {
+                        currentMaterial = objmats[mtlsplit[1]]; // look up the material's colour
+                    }
+                    catch (KeyNotFoundException) // if it doesn't exist, make it black
+                    {
+                        currentMaterial = new OBJMat { r = 0, g = 0, b = 0 };
+                    }
+                }
+
+
+                o++;
+            }
+
+            foreach(Vertex v in vertices)
+            {
+                writelines.Add(ConvertToCommand(v.line, v.material));
+            }
+
+            try
+            {
+                File.WriteAllLines(pathout, writelines);
+            }
+            catch(IOException)
+            {
+                progressBar1.Value = 100;
+                MessageBox.Show("Please try again, This is a common error \n (the file is being used by anotehr application)");
+                
+                return;
+            }
+            
+
+            Console.WriteLine("Faces: " + faceslength + " Verticies: " + verticylength + " VN: " + vnlength);
+            progressBar1.Value = 100;
+
+
+
+
+            //Console.WriteLine(stringout);
+
+        }
+
+        private string ConvertToCommand(string objline, OBJMat material)
+        {
+            string[] splitraw = objline.Split(' ');
+
+            if (splitraw[0] == "v")
+            {
+                //float x = float.Parse(splitraw[1]);
+                //float y = float.Parse(splitraw[2]);
+                //float z = float.Parse(splitraw[3]);
+
+
+
+                float colorx = material.r;
+                float colory = material.g;
+                float colorz = material.b;
+
+                //"particle dust " + color1 + " " + color2 + " " + color3 + " 1 " + cline + " 0 0 0 0 1 force"
+                // particle dust 0.8 0.009515 0.8 1 ~-0.530915 ~0.120492 ~0.930629 0 0 0 0 1 force
+
+                //List<OBJMat> objmats = new List<OBJMat>();
+                //objmats = GetMaterialsFromMTL(mtlline);
+                Console.WriteLine("BOOP, amount progress: " + amountprogress + " max verticys: " + verticylength + " extimated: " + updateprogress(amountprogress, verticylength));
+                amountprogress += 1;
+
+                progressBar1.Value = (int)(updateprogress(amountprogress, verticylength) * 100);
+                
+
+
+
+
+                //string comm = "particle dust " + colorx + " " + colory + " " + colorz + " 1 ~" + x + " ~" + y + " ~" + z + " 0 0 0 0 1 force";
+                string comm = "particle dust " + colorx + " " + colory + " " + colorz + " " + ParticleProperties.size + " ~" + splitraw[1] + " ~" + splitraw[2] + " ~" + splitraw[3] + " 0 0 0 0 1 force";
+
+                return comm;
+            }
+            else
+            {
+                return "";
+            }
+
+
+
+        }
+
+
+
+        private Dictionary<string, OBJMat> GetMaterialsFromMTL(string path)
+        {
+            List<string> AllLines = new List<string>();
+
+            Dictionary<string, OBJMat> mats = new Dictionary<string, OBJMat>();
+
+
+
+            foreach (string line in File.ReadAllLines(path))
+            {
+                AllLines.Add(line);
+
+
+            }
+
+            int i = 0;
+            foreach (string l in AllLines)
+            {
+                if (l.Contains("newmtl"))
+                {
+
+
+
+
+                    string[] splitline = l.Split(' ');
+                    string matnam = splitline[1];
+
+
+                    string colorline = AllLines[i + 3];
+
+                    string[] splitcolor = colorline.Split(' ');
+
+                    float red = float.Parse(splitcolor[1]);
+                    float green = float.Parse(splitcolor[2]);
+                    float blue = float.Parse(splitcolor[3]);
+
+
+
+
+
+
+
+                    mats.Add(matnam, new OBJMat() { r = red, g = green, b = blue });
+
+                    Console.WriteLine("----------");
+                    Console.WriteLine(matnam);
+                    Console.WriteLine(red);
+                    Console.WriteLine(green);
+                    Console.WriteLine(blue);
+                }
+
+                i++;
+            }
+
+            return mats;
+
+
+
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int scrollpos = trackBar1.Value;
+
+            ParticleProperties.size = float.Parse(scrollpos.ToString()) / 10;
+
+            textBox3.Text = ParticleProperties.size.ToString();
+            
+        }
+
+        private float updateprogress(int prog, int maxgrog)
+        {
+            float progfloat = float.Parse(prog.ToString());
+            float maxprogfloat = float.Parse(maxgrog.ToString());
+
+            float result = progfloat / maxprogfloat;
+
+            return result;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    //string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                    //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                    textBox4.Text = fbd.SelectedPath;
+                    Properties.Settings.Default["FileOutput"] = fbd.SelectedPath;
+                    Properties.Settings.Default.Save();
+                }
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            
+            Properties.Settings.Default["FileName"] = textBox5.Text;
+            Properties.Settings.Default.Save();
+        }
+    }
+
+    public class OBJMat
+    {
+        public float r;
+        public float b;
+        public float g;
+    }
+
+    public class Vertex
+    {
+        public string line;
+        public OBJMat material;
+    }
+
+    public static class ParticleProperties
+    {
+        public static float size = 1;
+    }
+
+}
